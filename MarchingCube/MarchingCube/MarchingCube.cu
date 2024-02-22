e2f609e19c8038c704c90176e6f297746fa7308d
92nodo-dev <92nodo30@gmail.com>
2024-02-22 15:13:25 +0900

    add vtk reader

--- MarchingCube/MarchingCube/MarchingCube.cu
+++ MarchingCube/MarchingCube/MarchingCube.cu
@@ -626,7 +626,17 @@ bool MarchingCube::initialize_cell()
 
 bool MarchingCube::get_vertices_by_vtu(std::string filepath)
 {
-
+	std::string line;
+	std::ifstream file(filepath);
+	bool isInsidePoint = false;
+	if (file.is_open()) {
+		while (std::getline(file, line)) {
+			if (line.find("<DataArray") != std::string::npos) {
+				// npos는 못찾은 경우
+				// 이런식으로 찾아들어가면될듯 line 별로
+			}
+		}
+	}
 }
 
 bool MarchingCube::find_grid_minmax()
